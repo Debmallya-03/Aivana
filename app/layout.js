e2a534198a72b07f8d624d4180b472babb977275
Debmallya-03 <debmallyabhandari4@gmail.com>
2025-07-26 23:37:49 +0530

    Set up User Auth and backend database

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
 import ParticleBackground from "@/components/floating-shapes";
 import Header from "@/components/header";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,12 +28,21 @@ export default function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
+            <ClerkProvider
+            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+            appearance={{
+              baseTheme: shadesOfPurple,
+            }}            
+            >
+            <ConvexClientProvider>
             <Header />
         <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
 
           <ParticleBackground />
           <Toaster richColors />
           {children}</main>
+          </ConvexClientProvider>
+          </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
